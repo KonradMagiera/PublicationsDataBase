39df1a9fe182b666b0b5fd9fe596533be8409358
KonradMagiera <k.magiera1@wp.pl>
2019-12-18 17:48:00 +0100

    CRUD for publications and files

---
+++ api/app.py
@@ -0,0 +1,397 @@
+from flask import Flask, render_template, request, redirect, url_for, send_file, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from datetime import datetime
+import os
+from os.path import isfile, join
+from uuid import uuid4
+import jwt
+import json
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+# CONFIG
+load_dotenv(verbose=True)
+app.config['UPLOAD_FOLDER'] = os.getenv("UPLOAD_FOLDER")
+UPLOAD_TIME = int(os.getenv("UPLOAD_TIME"))
+DOWNLOAD_TIME = int(os.getenv("DOWNLOAD_TIME"))
+JWT_SECRET = os.getenv("JWT_SECRET")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
+db = SQLAlchemy(app)
+
+class Publication(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(256), unique=True, nullable=False)
+	author = db.Column(db.String(128), unique=False, nullable=False)
+	publisher = db.Column(db.String(128), unique=False, nullable=False)
+	user = db.Column(db.String(32), unique=False, nullable=False)
+	filename = db.Column(db.String(128), unique=False, nullable=True)
+	pub_date = db.Column(db.DateTime, default=datetime.now, unique=False, nullable=True)
+
+
+	def __init__(self, title, author, publisher, user, filename=None, pub_date=None, id=None):
+		self.id = id
+		self.title = title
+		self.author = author
+		self.publisher = publisher
+		self.user = user
+		self.filename = filename
+		self.pub_date = pub_date
+
+	def get_all(self):
+		data = {
+			"id": self.id,
+			"title": self.title,
+			"author": self.author,
+			"publisher": self.publisher,
+			"user": self.user,
+			"filename": self.filename,
+			"pub_date": self.pub_date
+		}
+		return data
+	
+	def update(self, title=None, author=None, publisher=None, pub_date=None, filename=None):
+		if(title):
+			self.title = title
+		if(author):
+			self.author = author
+		if(publisher):
+			self.publisher = publisher
+		if(pub_date):
+			self.pub_date = pub_date
+		if(filename):
+			self.filename = filename
+
+class File(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	pub_id = db.Column(db.Integer, unique=False, nullable=False)
+	filename = db.Column(db.String(128), unique=False, nullable=False)
+
+	def __init__(self, pub_id, filename, id=None):
+		self.id = id
+		self.pub_id = pub_id
+		self.filename = filename
+
+	def get_all(self):
+		data = {
+			"id": self.id,
+			"pub_id": self.pub_id,
+			"filename": self.filename
+		}
+		return data
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	user = db.Column(db.String(32), unique=True, nullable=False)
+	password = db.Column(db.String(64), unique=False, nullable=False)
+
+db.create_all()
+
+try:
+	user = User(user="admin", password="admin")
+	db.session.add(user)
+	db.session.commit()
+except IntegrityError:
+	db.session.rollback()
+
+try:
+	user = User(user="magierak", password="magierak")
+	db.session.add(user)
+	db.session.commit()
+except IntegrityError:
+	db.session.rollback()
+
+try:
+	pub = Publication(title="Title", author="Author", publisher="Publisher", user="admin", filename="Filename")
+	db.session.add(pub)
+	db.session.commit()
+except IntegrityError:
+	db.session.rollback()
+
+try:
+	pub = Publication(title="Title1", author="Author1", publisher="Publisher1", user="admin", filename="Filename1")
+	db.session.add(pub)
+	db.session.commit()
+except IntegrityError:
+	db.session.rollback()
+
+try:
+	pub = Publication(title="Title2", author="Author2", publisher="Publisher2", user="magierak", filename="Filename2")
+	db.session.add(pub)
+	db.session.commit()
+except IntegrityError:
+	db.session.rollback()
+
+try:
+	pub = Publication(title="Title3", author="Author", publisher="Publisher1", user="magierak", filename="Filename3")
+	db.session.add(pub)
+	db.session.commit()
+except IntegrityError:
+	db.session.rollback()
+
+try:
+	pub = Publication(title="Title4", author="Author2", publisher="Publisher2", user="admin", filename="Filename4")
+	db.session.add(pub)
+	db.session.commit()
+except IntegrityError:
+	db.session.rollback()
+
+
+@app.route('/login', methods=['POST'])
+def login():
+	data = request.get_json()
+	headers = request.headers.get('Authorization')
+	print(headers, flush=True)
+	if(("username" not in data) or ("password" not in data)):
+		return "send username and password", 401
+	user = data["username"]
+	password = data["password"]
+	user_db = User.query.filter_by(user=user).first()
+	if(user_db == None):
+		return "user does not exist", 401
+	elif(user_db.password == password):
+		return "zalogowano", 200
+	else:
+		return "bad credentials", 401
+
+@app.route('/logout', methods=['POST'])
+def logout():
+	data = request.get_json()
+	if("username" in data):
+		return "logged out", 200
+	elif("username" not in data):
+		return "logged out with troubles", 200
+	else:
+		return "unexpected error", 500
+
+@app.route('/', methods=['GET'])
+@app.route('/publications', methods=['GET'])
+def publications():
+	data = request.get_json()
+	#JWT
+	if('username' not in data):
+		data = {"message": "username was not provided"}
+		return jsonify(data), 401
+	user = data['username']
+	publ = Publication.query.filter_by(user=user).all()
+	tmp_pub = list()
+	for p in publ:
+		tmp_pub.append(p.get_all())
+	data = { "publication": tmp_pub }
+	return jsonify(data), 200
+
+@app.route('/', methods=['POST'])
+@app.route('/publications', methods=['POST'])
+def publications_add():
+	#JWT
+	data = request.get_json() #username, jwt, author, publisher, title
+	if(("username" not in data) or ("title" not in data) or ("author" not in data) or ("publisher" not in data)):
+		msg = {"message": "missing json parameters"}
+		return jsonify(msg)
+	
+	pub = Publication.query.filter_by(title=data['title']).first()
+	if(pub != None):
+		msg = {"message": "publication exists"}
+		return jsonify(msg), 401
+	
+	error = False
+	id = None
+	try:
+		pub = None
+		if(("date" in data) and data['date'] != ''):
+			format_str = '%Y-%m-%d'
+			datetime_obj = datetime.strptime(data['date'], format_str)
+			pub = Publication(title=data['title'], author=data['author'], publisher=data['publisher'], user=data['username'], pub_date=datetime_obj)
+			db.session.add(pub)
+			db.session.commit()
+		else:
+			pub = Publication(title=data['title'], author=data['author'], publisher=data['publisher'], user=data['username'])
+			db.session.add(pub)
+			db.session.commit()
+		id = pub.get_all()
+		id = id['id']
+	except IntegrityError:
+		error = True
+		db.session.rollback()
+
+	msg = {}
+	if(error):
+		msg = {"message": "add failed"}
+		return jsonify(msg), 401
+	else:
+		msg = {"message": "publication added", "id": id}
+		return jsonify(msg), 200
+
+@app.route('/publications/<pid>', methods=['GET'])
+def publicationspid(pid):
+	data = request.get_json()
+	#JWT
+	if("username" not in data):
+		data = {"error": "username not provided"}
+		return jsonify(data), 401
+	publ = Publication.query.filter_by(id=pid, user=data['username']).first()
+	tmp_pub = list()
+	tmp_pub.append(publ.get_all())
+	data = { "publication": tmp_pub }
+	return jsonify(data), 200
+
+@app.route('/publications/<pid>', methods=['PUT'])
+def publications_id_update(pid):
+	data = request.get_json()
+	if(('id' not in data) or ('username' not in data) or ("title" not in data) or ("author" not in data) or ("publisher" not in data)):
+		msg = {"message": "id not provided"}
+		return jsonify(msg), 401
+	pub = Publication.query.filter_by(id=data['id'], user=data['username']).first()
+	if(pub == None):
+		msg = {"message": "publication doesnt exists"}
+		return jsonify(msg), 401
+	date = None
+	if(data['date'] != ''):
+		format_str = '%Y-%m-%d'
+		date = datetime.strptime(data['date'], format_str)
+	pub.update(title=data['title'], author=data['author'], publisher=data['publisher'], pub_date=date)
+	msg = {"message": "Before commit"}
+	try:
+		db.session.commit()
+		msg = {"message": "succesfully updated"}
+		return jsonify(msg), 200
+	except IntegrityError:
+		db.session.rollback()
+		msg = {"message": "title already exists"}
+		return jsonify(msg), 401
+
+@app.route('/publications/<pid>', methods=['DELETE'])
+def publications_id_delete(pid):
+	data = request.get_json()
+	#JWT
+	if('username' not in data):
+		msg = {"message": "Misisng id or username"}
+		return jsonify(msg), 401
+	pubquery = Publication.query.filter_by(id=pid, user=data['username'])
+	if(pubquery.count() != 1):
+		msg = {"message": f"publication with id {pid} doesnt exist"}
+		return jsonify(msg), 401
+	delete_all_pub_files(pid)	
+	try:
+		pubquery.delete()
+		db.session.commit()
+		msg = {"message": "Successfully deleted"}
+		return jsonify(msg), 200
+	except IntegrityError:
+		db.session.rollback()
+		msg = {"message": "Failed to delete"}
+		return jsonify(msg), 401
+
+@app.route('/publications/<pid>/files', methods=['GET'])
+def publicationspid_files(pid):
+	data = request.get_json()
+	if("username" not in data):
+		msg = {"message": "user not specified"}
+		return jsonify(msg), 401
+	files = File.query.filter_by(pub_id=pid).all()
+	tmp_files = list()
+	for f in files:
+		tmp_files.append(f.get_all())
+	data = { "publication": tmp_files }
+	return jsonify(data), 200
+
+@app.route('/publications/<pid>/files', methods=['POST'])
+def files_add(pid):
+	files = request.files.get('file')
+	extension = files.filename.split(".")[-1]
+	msg = {"message": "before save"}
+	if files and extension == 'pdf':
+		filepath = os.path.join(app.config['UPLOAD_FOLDER'], pid)
+		if not os.path.exists(filepath):
+			os.mkdir(filepath)
+		filepath = os.path.join(filepath, files.filename)
+
+		pub = Publication.query.filter_by(id=pid).first()
+		if(pub == None):
+			msg = {"message": "publication not found"}
+			return jsonify(msg), 401
+
+		filequery = File.query.filter_by(pub_id=int(pid), filename=files.filename).first()
+		if(filequery != None):
+			msg = {"message": "file with specific name already added to publication"}
+			return jsonify(msg), 401
+		try:
+			filequery = File(filename=files.filename, pub_id=int(pid))
+			db.session.add(filequery)
+			db.session.commit()
+			files.save(filepath)
+			msg = {"message": "file added"}
+			return jsonify(msg), 200
+		except IntegrityError:
+			db.session.rollback()
+			msg = {"message": "error while adding file"}
+			return jsonify(msg), 401
+	else:
+		msg = {"message": "error while saving file"}
+		return jsonify(msg), 401
+
+@app.route('/publications/<pid>/files/<fid>', methods=['GET'])
+def filesfid(pid, fid):
+	files = File.query.filter_by(id=fid, pub_id=pid).first()
+	if(files == None):
+		msg = {"message": "Could not find file for given ids"}
+		return jsonify(msg), 401
+	
+	filename = files.get_all()
+	filename = filename['filename']
+	filepath = app.config['UPLOAD_FOLDER']
+	filepath = os.path.join(filepath, pid)
+	filepath = os.path.join(filepath, filename)
+	send = send_file(filepath, attachment_filename=filename, as_attachment=True)
+	return send, 200
+
+@app.route('/publications/<pid>/files/<fid>', methods=['DELETE'])
+def file_delete(pid, fid):
+	filequery = File.query.filter_by(id=fid, pub_id=pid)
+	if(filequery.count() != 1):
+		msg = {"messaage": "file not found"}
+		return jsonify(msg), 401
+	try:
+		filepath = os.path.join(app.config['UPLOAD_FOLDER'], pid)
+		files = File.query.filter_by(id=fid, pub_id=pid).first()
+		files = files.get_all()
+		filepath = os.path.join(filepath, files['filename'])
+		print(filepath, flush=True)
+		filequery.delete()
+		db.session.commit()
+		msg = {"message": "Successfully deleted"}
+		os.remove(filepath)
+		return jsonify(msg), 200
+	except IntegrityError:
+		db.session.rollback()
+		msg = {"message": "Failed to delete"}
+		return jsonify(msg), 401
+
+def delete_all_pub_files(pid):
+	files = File.query.filter_by(pub_id=pid).all()
+	file_ids = list()
+	filenames = list()
+	for f in files:
+		tmp = f.get_all()
+		file_ids.append(tmp['id'])
+		filenames.append(tmp['filename'])
+
+	print(file_ids, flush=True)
+	for fid, filename in zip(file_ids, filenames):
+		filequery = File.query.filter_by(id=int(fid), pub_id=int(pid))
+		if(filequery.count() != 1):
+			msg = {"message": "file not found"}
+			return jsonify(msg), 401
+		filepath = os.path.join(app.config['UPLOAD_FOLDER'], str(pid))
+		filepath = os.path.join(filepath, filename)
+		try:
+			filequery.delete()
+			db.session.commit()
+			msg = {"message": "Successfully deleted"}
+			os.remove(filepath)
+		except:
+			db.session.rollback()
+			msg = {"message": "Failed to delete"}
+	return "TMP"
---
+++ client/app.py
@@ -0,0 +1,213 @@
+from flask import Flask, render_template, request, redirect, url_for, make_response, send_file, jsonify, Response
+import os
+from os.path import isfile, join
+from uuid import uuid4
+import jwt
+import requests
+import datetime
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
+app = Flask(__name__)
+UPLOAD_TIME = int(os.getenv("UPLOAD_TIME"))
+DOWNLOAD_TIME = int(os.getenv("DOWNLOAD_TIME"))
+JWT_SECRET = os.getenv("JWT_SECRET")
+app.config['CURRENT_USER'] = ''
+
+@app.route('/', methods=['GET'])
+def index():
+	session_id = request.cookies.get('session_id')
+	user_tmp = app.config['CURRENT_USER']
+	if(user_tmp == ''):
+		response = make_response(redirect(url_for('login')))
+		response.set_cookie('session_id', '', max_age=0)
+	else:
+		response = redirect(url_for('profile', username=app.config['CURRENT_USER']) if session_id else url_for('render_login'))
+	return response
+
+@app.route('/login', methods=['GET'])
+def render_login():
+	error = ''
+	if('error' in request.args):
+		error = request.args['error']
+	return render_template("login.html", error=error)
+
+@app.route('/login', methods=['POST'])
+def login():
+	user = request.form.get('username')
+	password = request.form.get('password')
+	token = {"username": user, "password": password}
+	headers= {"Authorization": user + ":" + password}
+	response = requests.post("http://api:5000/login", json={"username": user, "password": password}, headers=headers)	
+	if(response.status_code == 200):
+		app.config['CURRENT_USER'] = user
+		resp = make_response(redirect(url_for('render_profile')))
+		session_id = str(uuid4())
+		resp.set_cookie('session_id', session_id, httponly=True)
+		return resp
+	return redirect(url_for('login', error=response.text))
+
+@app.route('/profile', methods=['GET'])
+def render_profile():
+	if('session_id' in request.cookies):
+		user = app.config['CURRENT_USER']
+		return render_template("profile.html", username=user)
+	else:
+		app.config['CURRENT_USER'] = ''
+		return redirect(url_for('login', error="Session expired"))
+
+
+@app.route('/profile', methods=['POST'])
+def profile():
+	if request.form['btn'] == 'Logout':
+		response = requests.post("http://api:5000/logout", json={"username": app.config['CURRENT_USER']})
+		if(response.status_code == 200):
+			resp = make_response(redirect(url_for('login')))
+			resp.set_cookie('session_id', '', max_age=0)
+			app.config['CURRENT_USER'] = ''
+			return resp
+		else:
+			resp = make_response(redirect(url_for('login', error="error occured")))
+			resp.set_cookie('session_id', '', max_age=0)
+			app.config['CURRENT_USER'] = ''
+			return resp
+	else:
+		return redirect(url_for('render_publications'))
+
+@app.route('/publications', methods=['GET'])
+def render_publications():
+	response = requests.get("http://api:5000/publications", json={"username": app.config['CURRENT_USER']})
+	files = list()
+	data = response.json()
+	if("publication" not in data):
+		files = None
+	else:	
+		data = data['publication']
+		for pub in data:
+			files.append({'id': pub['id'], 'title': pub['title']})
+	return render_template('publications.html', publications=files)
+
+@app.route('/publications', methods=['POST'])
+def publications_back():
+	if(request.form['btn'] =='Back'):
+		return redirect(url_for('render_profile'))
+	else:
+		#Add
+		return redirect(url_for('render_publications_add'))
+
+@app.route('/publications/add', methods=['GET'])
+def render_publications_add():
+	return render_template('add_publication.html')
+
+
+@app.route('/publications/add', methods=['POST'])
+def publications_add():
+	if(request.form['btn'] == 'Save'):
+		title = request.form['title']
+		author = request.form['author']
+		publisher = request.form['publisher']
+		date = request.form['date']
+		file = request.files.get('file')
+		files = {'file': (file.filename, file, 'application/pdf')}
+		
+		response = requests.post('http://api:5000/publications', json={"username": app.config['CURRENT_USER'], "title": title, "author": author, "publisher": publisher, "date": date})
+		if(file.filename != ''):
+			data = response.json()
+			if('id' in data):
+				id = data['id']
+				headers = {'Content-type': 'multipart/form-data'}
+				response = requests.post('http://api:5000/publications/' + str(id) + "/files", files=files)
+
+	return redirect(url_for('render_publications'))
+
+
+@app.route('/publications/<id>', methods=['GET'])
+def render_publications_id(id):
+	response = requests.get('http://api:5000/publications/' + id, json={"username": app.config['CURRENT_USER']})
+	data = response.json()
+	if((response.status_code != 200) or ("publication" not in data)):
+		return redirect(url_for("render_publications"))
+	data = data['publication']
+	data = data[0]
+	response = requests.get('http://api:5000/publications/'+ id + "/files", json={"username": app.config['CURRENT_USER']})
+	files = response.json()
+	files = files['publication']
+	if(len(files) == 0):
+		files = None
+	return render_template('publication.html', publication=data, files=files)
+
+
+@app.route('/publications/<id>', methods=['POST'])
+def publications_id_post(id):
+	if(request.form['btn'] == 'Back'):
+		return redirect(url_for('render_publications'))
+	elif(request.form['btn'] == 'Edit'):
+		return redirect(url_for('render_publication_id_edit', id=id))
+	elif(request.form['btn'] == 'Add file'):
+		file = request.files.get('file')
+		files = {'file': (file.filename, file, 'application/pdf')}
+		if(file.filename != ''):
+				headers = {'Content-type': 'multipart/form-data'}
+				response = requests.post('http://api:5000/publications/' + str(id) + "/files", files=files)
+		return redirect(url_for('render_publications_id', id=id))
+
+
+@app.route('/publications/<id>/edit', methods=['GET'])
+def render_publication_id_edit(id):
+	response = requests.get('http://api:5000/publications/' + id, json={"username": app.config['CURRENT_USER']})
+	data = response.json()
+	if((response.status_code != 200) or ("publication" not in data)):
+		return redirect(url_for("render_publications_id", id=id))
+	data = data['publication']
+	data = data[0]
+	date = data['pub_date']
+	date = date[5:-13]
+	day = date[0:2]
+	month = {
+		'Jan' : 1,
+        'Feb' : 2,
+        'Mar' : 3,
+        'Apr' : 4,
+        'May' : 5,
+        'Jun' : 6,
+        'Jul' : 7,
+        'Aug' : 8,
+        'Sep' : 9, 
+        'Oct' : 10,
+        'Nov' : 11,
+        'Dec' : 12}
+	tmp_mon = date[3:6]
+	month = month[tmp_mon]
+	year = date[7:11]
+	data['pub_date'] = str(year) + '-' + str(month) + '-' + day
+	return render_template('edit_publication.html', publication=data)
+
+
+@app.route('/publications/<id>/edit', methods=['POST'])
+def send_publications_id_edit(id):
+	if(request.form['btn'] == 'Save'):
+		title = request.form['title']
+		author = request.form['author']
+		publisher = request.form['publisher']
+		date = request.form['date']
+		response = requests.put('http://api:5000/publications/' + id, json={"username": app.config['CURRENT_USER'],"id": id, "title": title, "author": author, "publisher": publisher, "date":date})
+	return redirect(url_for('render_publications_id', id=id))
+
+
+@app.route('/publications/<id>/delete', methods=['GET'])
+def publications_id_delete(id):
+	response = requests.delete('http://api:5000/publications/' + id, json={"username": app.config['CURRENT_USER']})
+	return redirect(url_for('render_publications'))
+
+
+@app.route('/publications/<pid>/files/<fid>', methods=['GET'])
+def file_download(pid, fid):
+	response = requests.get('http://api:5000/publications/' + pid + "/files/" + fid)
+	resp = Response(response=response.content, content_type='application/pdf')
+	return resp
+
+@app.route('/publications/<pid>/files/<fid>/delete', methods=['GET'])
+def file_delete(pid, fid):
+	response = requests.delete('http://api:5000/publications/' + pid + "/files/" + fid)
+	print(response.text, flush=True)
+	return redirect(url_for('render_publications_id', id=pid))
