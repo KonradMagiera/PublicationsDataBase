97e02c9992ad7df066a0a489a485d7e1186d0dc0
KonradMagiera <k.magiera1@wp.pl>
2019-12-23 16:49:55 +0100

    test

--- ClientJavaFX/src/clientjavafx/ClientJavaFX.java
+++ ClientJavaFX/src/clientjavafx/ClientJavaFX.java
@@ -1,101 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clientjavafx;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import view.MainController;
 
-/**
- *
- * @author Konrad
- */
+
 public class ClientJavaFX extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                try {
-                    System.out.println("Hello World!");
-                    HttpURLConnection connection = (HttpURLConnection) new URL("http://192.168.99.100:5000/login").openConnection();
-                    connection.setDoOutput(true);
-                    connection.setRequestMethod("POST");
-                    
-                    // JWT generate
-                    connection.setRequestProperty("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiJhZG1pbiIsImV4cCI6MTU3NzE1MjU2MX0.aMxM0HbnIm_V_sw42OZh7BqgJAiWZop97VF6menfp0c");
-                    InputStream response = connection.getInputStream();
-                    String responseMsg = convertStreamToString(response);
-                    System.out.println(responseMsg);
-                } catch (MalformedURLException ex) {
-                    System.out.println("1111111111111");
-                    Logger.getLogger(ClientJavaFX.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    System.out.println("2222222222");
-                    Logger.getLogger(ClientJavaFX.class.getName()).log(Level.SEVERE, null, ex);
-                }
-
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage)  throws IOException{
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/clientjavafx/fxml/main.fxml"));;
+        //System.out.println(loader.getRoot().toString());
+	GridPane pane = loader.load();
+	//Scene scene = new Scene(pane);
+        primaryStage.setTitle("Publications");
+        //primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
-    }
-
-    private static String convertStreamToString(InputStream is) {
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
     }
 }
---
+++ ClientJavaFX/src/view/Login.java
@@ -0,0 +1,27 @@
+package view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+public class Login implements Initializable {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @FXML
+    private void signIn(ActionEvent event) {
+        
+    }
+    
+}
