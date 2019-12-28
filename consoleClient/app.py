62874e5a5bc2a5561b1aa3b5e88bef5ecda23ad1
KonradMagiera <k.magiera1@wp.pl>
2019-12-28 07:54:53 +0100

    cleaning

--- consoleClient/app.py
+++ consoleClient/app.py
@@ -12,7 +12,7 @@ REQUEST_CREDENTIALS_EXPIRE = int(os.getenv("REQUEST_CREDENTIALS_EXPIRE"))
 PUBLICATIONS_ACCESS = int(os.getenv("PUBLICATIONS_ACCESS"))
 CURRENT_USER = ""
 SESSION_ID = ""
-
+api = "http://localhost:5000/"
 
 
 def start_menu():
@@ -52,7 +52,7 @@ def file_options(id, filename):
 
 def create_jwt(expire_time):
 	token = {"username": CURRENT_USER, "session_id": SESSION_ID, "exp": datetime.now() + timedelta(seconds=expire_time)}
-	token = jwt.encode(token, JWT_SECRET, algorithm='HS256')
+	token = jwt.encode(token, JWT_SECRET, algorithm="HS256")
 	return token
 
 def login():
@@ -60,9 +60,9 @@ def login():
     username = input()
     password = getpass.getpass("Password: ")
     token = {"username": username, "password": password, "exp": datetime.now() + timedelta(seconds=REQUEST_CREDENTIALS_EXPIRE)}
-    token = jwt.encode(token, JWT_SECRET, algorithm='HS256')
+    token = jwt.encode(token, JWT_SECRET, algorithm="HS256")
     headers= {"Authorization": token}
-    response = requests.post("http://localhost:5000/login", headers=headers)
+    response = requests.post(api + "login", headers=headers)
 
     if(response.status_code == 200):
         token_decode = response.headers.get("Authorization")
@@ -84,7 +84,7 @@ def login():
 
 def logout():
     headers= {"Authorization": create_jwt(REQUEST_CREDENTIALS_EXPIRE)}
-    response = requests.post("http://localhost:5000/logout", headers=headers)
+    response = requests.post(api + "logout", headers=headers)
     if(response.status_code == 200):
         return "", ""
     else:
@@ -93,15 +93,15 @@ def logout():
 
 def publications():
     headers= {"Authorization": create_jwt(PUBLICATIONS_ACCESS)}
-    response = requests.get("http://localhost:5000/publications", headers=headers)
+    response = requests.get(api + "publications", headers=headers)
     pubs = list()
     data = response.json()
     if(("publication" not in data) or (response.status_code != 200)):
         pubs = None
     else:	
-        data = data['publication']
+        data = data["publication"]
         for pub in data:
-            pubs.append({'id': pub['id'], 'title': pub['title']})
+            pubs.append({"id": pub["id"], "title": pub["title"]})
     return pubs
 
 def print_publications(pubs):
@@ -130,11 +130,11 @@ def add_publication():
         if(len(date) == 10 or date == ""):
             break
     headers= {"Authorization": create_jwt(PUBLICATIONS_ACCESS)}
-    response = requests.post('http://localhost:5000/publications', json={"title": title, "author": author, "publisher": publisher, "date": date}, headers=headers)
+    response = requests.post(api + "publications", json={"title": title, "author": author, "publisher": publisher, "date": date}, headers=headers)
     data = response.json()
     id = -1
-    if('id' in data):
-        id = data['id']
+    if("id" in data):
+        id = data["id"]
     return id
 
 def edit_publication(id):
@@ -175,11 +175,11 @@ def edit_publication(id):
             date = build_date(date)
             break
     headers= {"Authorization": create_jwt(PUBLICATIONS_ACCESS)}
-    requests.put('http://localhost:5000/publications/' + str(id), json={"id": str(id), "title": title, "author": author, "publisher": publisher, "date":date}, headers=headers)
+    requests.put(api + "publications/" + str(id), json={"id": str(id), "title": title, "author": author, "publisher": publisher, "date":date}, headers=headers)
     
 def delete_publication(id):
     headers= {"Authorization": create_jwt(PUBLICATIONS_ACCESS)}
-    requests.delete('http://localhost:5000/publications/' + str(id), headers=headers)
+    requests.delete(api + "publications/" + str(id), headers=headers)
 
 def print_pub(id):
     data = get_pub_data(id)
@@ -198,19 +198,19 @@ def print_pub(id):
 
 def get_pub_data(id):
     headers= {"Authorization": create_jwt(PUBLICATIONS_ACCESS)}
-    response = requests.get('http://localhost:5000/publications/' + str(id), headers=headers)
+    response = requests.get(api + "publications/" + str(id), headers=headers)
     data = response.json()
     if((response.status_code != 200) or ("publication" not in data)):
         return None
-    data = data['publication']
+    data = data["publication"]
     data = data[0]
     return data
 
 def get_pub_files(id):
     headers= {"Authorization": create_jwt(PUBLICATIONS_ACCESS)}
-    response = requests.get('http://localhost:5000/publications/'+ str(id) + "/files", headers=headers)
+    response = requests.get(api + "publications/" + str(id) + "/files", headers=headers)
     files = response.json()
-    files = files['publication']
+    files = files["publication"]
     if(len(files) == 0):
         files = None
     return files
@@ -219,53 +219,55 @@ def build_date(pub_date):
     date = pub_date[5:-13]
     day = date[0:2]
     month = {
-		'Jan' : 1,
-        'Feb' : 2,
-        'Mar' : 3,
-        'Apr' : 4,
-        'May' : 5,
-        'Jun' : 6,
-        'Jul' : 7,
-        'Aug' : 8,
-        'Sep' : 9, 
-        'Oct' : 10,
-        'Nov' : 11,
-        'Dec' : 12}
+		"Jan" : 1,
+        "Feb" : 2,
+        "Mar" : 3,
+        "Apr" : 4,
+        "May" : 5,
+        "Jun" : 6,
+        "Jul" : 7,
+        "Aug" : 8,
+        "Sep" : 9, 
+        "Oct" : 10,
+        "Nov" : 11,
+        "Dec" : 12}
     tmp_mon = date[3:6]
     month = month[tmp_mon]
     year = date[7:11]
-    date = str(year) + '-' + str(month) + '-' + day
+    date = str(year) + "-" + str(month) + "-" + day
     return date
 
 def add_file(id):
     print("File (path): ", end="")
     file = input()
+    if(file == ""):
+        return True
     try:
         file = open(file, "rb")
     except IOError:
         return False
     filename = os.path.basename(file.name)
-    files = {'file': (filename, file, 'application/pdf')}
-    if(filename != ''):
+    files = {"file": (filename, file, "application/pdf")}
+    if(filename != ""):
             headers= {"Authorization": create_jwt(PUBLICATIONS_ACCESS)}
-            response = requests.post('http://localhost:5000/publications/' + str(id) + "/files", files=files, headers=headers)
+            response = requests.post(api + "publications/" + str(id) + "/files", files=files, headers=headers)
             if(response.status_code == 201):
                 return True
     return False
 
 def delete_file(pid, fid):
     headers= {"Authorization": create_jwt(PUBLICATIONS_ACCESS)}
-    requests.delete('http://localhost:5000/publications/' + str(pid) + "/files/" + str(fid), headers=headers)
+    requests.delete(api + "publications/" + str(pid) + "/files/" + str(fid), headers=headers)
 
 def download_file(pid, fid):
     headers= {"Authorization": create_jwt(PUBLICATIONS_ACCESS)}
-    response = requests.get('http://localhost:5000/publications/' + str(pid) + "/files/" + str(fid), headers=headers)
+    response = requests.get(api + "publications/" + str(pid) + "/files/" + str(fid), headers=headers)
     if(response.status_code != 200):
         return False
 
-    d = response.headers['content-disposition']
+    d = response.headers["content-disposition"]
     fname = re.findall("filename=(.+)", d)[0]
-    with open(fname, 'wb') as f:
+    with open(fname, "wb") as f:
         f.write(response.content)
     return True
 
